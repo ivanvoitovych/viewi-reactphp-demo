77f993211eaecb22dea1078ffa07f49e7a35ecad
Ivan Voitovych <col606v@gmail.com>
2022-01-16 11:39:38 +0200

    StaticFilesMiddleware

--- App/Middleware/StaticFilesMiddleware.php
+++ App/Middleware/StaticFilesMiddleware.php
@@ -6,35 +6,40 @@ use React\Http\Message\Response;
 
 class StaticFilesMiddleware
 {
-    public static function get(string $dir)
+    private string $directory;
+
+    public function __construct(string $directory)
     {
-        return function (\Psr\Http\Message\ServerRequestInterface $request, callable $next) use ($dir) {
-            $filePath = $request->getUri()->getPath();
-            $file = $dir . '/public' . $filePath;
-            if (file_exists($file) && !is_dir($file)) {
-                $fileExt = pathinfo($file, PATHINFO_EXTENSION);
-                $contentType = 'text/text';
-                switch ($fileExt) {
-                    case 'js': {
-                            $contentType = 'application/javascript';
-                            break;
-                        }
-                    case 'json': {
-                            $contentType = 'application/json';
-                            break;
-                        }
-                    case 'css': {
-                            $contentType = 'text/css';
-                            break;
-                        }
-                    case 'ico': {
-                            $contentType = 'image/x-icon';
-                            break;
-                        }
-                }
-                return new Response(200, ['Content-Type' => $contentType], file_get_contents($file));
+        $this->directory = $directory;
+    }
+
+    public function __invoke(\Psr\Http\Message\ServerRequestInterface $request, callable $next)
+    {
+        $filePath = $request->getUri()->getPath();
+        $file = $this->directory . '/public' . $filePath;
+        if (file_exists($file) && !is_dir($file)) {
+            $fileExt = pathinfo($file, PATHINFO_EXTENSION);
+            $contentType = 'text/text';
+            switch ($fileExt) {
+                case 'js': {
+                        $contentType = 'application/javascript';
+                        break;
+                    }
+                case 'json': {
+                        $contentType = 'application/json';
+                        break;
+                    }
+                case 'css': {
+                        $contentType = 'text/css';
+                        break;
+                    }
+                case 'ico': {
+                        $contentType = 'image/x-icon';
+                        break;
+                    }
             }
-            return $next($request);
-        };
+            return new Response(200, ['Content-Type' => $contentType], file_get_contents($file));
+        }
+        return $next($request);
     }
 }
