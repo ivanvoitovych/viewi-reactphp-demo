3823d470c0c2a0f9a302bd576e1170e612d724a4
Ivan Voitovych <col606v@gmail.com>
2022-01-16 14:35:23 +0200

    RequestsHandlerMiddleware: run within container, http context, redirect

--- App/Middleware/RequestsHandlerMiddleware.php
+++ App/Middleware/RequestsHandlerMiddleware.php
@@ -2,14 +2,19 @@
 
 namespace App\Middleware;
 
+use App\Http\ReactHttpContext;
 use Psr\Http\Message\ServerRequestInterface;
 use React\Http\Message\Response;
 use Throwable;
+use Viewi\BaseComponent;
+use Viewi\DI\Container;
 use Viewi\Routing\Router;
+use Viewi\WebComponents\IHttpContext;
 
 class RequestsHandlerMiddleware
 {
-    public function __invoke(ServerRequestInterface $request) {
+    public function __invoke(ServerRequestInterface $request)
+    {
         try {
             // print_r([$request->getUri()->getPath(), $request->getMethod(), $request->getQueryParams()]);
             $match = Router::resolve($request->getUri()->getPath(), $request->getMethod());
@@ -22,28 +27,40 @@ class RequestsHandlerMiddleware
                     "Page not Found!\n"
                 );
             }
-    
+
             /** @var RouteItem $route */
             $route = $match['route'];
-            $action = $route->action;            
+            $action = $route->action;
             if (is_callable($action) && !is_string($action)) {
                 // print_r([$request->getUri()->getPath(), $request->getMethod(), $action, $match['params']]);
-                if($match['params'])
-                {
+                if ($match['params']) {
                     $request = $request->withAttribute('params', $match['params']);
                 }
                 return $action($request);
-            }            
+            }
+            /** @var BaseComponent $instance */
             $instance = new $action();
-            $viewiResponse = $instance($match['params']);
+            $container = new Container();
+            $httpContext = new ReactHttpContext($request);
+            $container->set(IHttpContext::class, $httpContext);
+            $viewiResponse = $instance($match['params'], $container);
+            $headers = $httpContext->getResponseHeaders();
+            if(isset($headers['Location']))
+            {
+                return new Response(
+                    302,
+                    $headers                    
+                );
+            }
             // $viewiResponse = Router::handle($request->getUri()->getPath(), $request->getMethod(), $request->getQueryParams());
             // print_r(['response', $viewiResponse]);
+            // print_r(['context', $request->getUri()->getPath(), $request->getMethod(), $httpContext->getResponseHeaders()]);
             if ($viewiResponse instanceof \Viewi\WebComponents\Response) {
                 return new Response(
                     $viewiResponse->StatusCode,
                     array(
                         'Content-Type' => 'text/html'
-                    ),
+                    ) + $headers,
                     $viewiResponse->Content
                 );
             } else if (is_string($viewiResponse)) {
@@ -51,7 +68,7 @@ class RequestsHandlerMiddleware
                     200,
                     array(
                         'Content-Type' => 'text/html'
-                    ),
+                    ) + $headers,
                     $viewiResponse
                 );
             } else { // json
@@ -59,7 +76,7 @@ class RequestsHandlerMiddleware
                     200,
                     array(
                         'Content-Type' => 'application/json'
-                    ),
+                    ) + $headers,
                     json_encode($viewiResponse)
                 );
             }
