65e72ebacc5746aa4774c3de24156f7cff8795a3
Ivan Voitovych <col606v@gmail.com>
2022-01-26 09:53:47 +0200

    server.php

--- server.php
+++ server.php
@@ -9,6 +9,7 @@ use App\Middleware\StaticFilesMiddleware;
 use Components\Models\PostModel;
 use Psr\Http\Message\ServerRequestInterface;
 use React\EventLoop\Loop;
+use React\Http\Message\Response;
 use React\Promise\Promise;
 use Viewi\Routing\Route;
 
@@ -25,9 +26,14 @@ Viewi\Routing\Router::register('get', '/api/posts/{id}', function (ServerRequest
     return new RawJsonResponse($post);
 });
 
-Viewi\Routing\Router::register('get', '/api/posts/{id}/async', function (ServerRequestInterface $request) {
+Viewi\Routing\Router::register('get', '/api/posts/{id}/async/{ms?}', function (ServerRequestInterface $request) {
     return new Promise(function ($resolve, $reject) use ($request) {
-        Loop::addTimer((float)1 + (rand(1, 20) / 100), function () use ($resolve, $request) {
+        $ms = $request->getAttribute('params')['ms'] ?? 1000;
+        if ($ms > 5000) // we don't want it to be more than 5 sec
+        {
+            $ms = 5000;
+        }
+        Loop::addTimer($ms / 1000 + (rand(1, 20) / 100), function () use ($resolve, $request) {
             $postId = $request->getAttribute('params')['id'] ?? 0;
             $post = new PostModel();
             $post->Id = $postId;
@@ -42,8 +48,22 @@ Viewi\Routing\Router::register('get', '/api/posts/{id}/async', function (ServerR
 
 Viewi\Routing\Router::register('post', '/api/authorization/session', function (ServerRequestInterface $request) {
     return new Promise(function ($resolve, $reject) use ($request) {
-        Loop::addTimer(0.5, function () use ($resolve, $request) {
+        Loop::addTimer(0.2, function () use ($resolve, $request) {
             $response = new RawJsonResponse(['session' => '000-1111-2222']);
+            $resolve($response);
+        });
+    });
+});
+
+Viewi\Routing\Router::register('post', '/api/authorization/token/{valid}', function (ServerRequestInterface $request) {
+    return new Promise(function ($resolve, $reject) use ($request) {
+        Loop::addTimer(0.2, function () use ($resolve, $request) {
+            $valid = $request->getAttribute('params')['valid'] === 'true';
+            if (!$valid) {
+                $resolve(new Response(401, [], ''));
+                return;
+            }
+            $response = new RawJsonResponse(['token' => 'base64string']);
             $resolve($response);
         });
     });
