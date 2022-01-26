637d5604d771fb72d091bb496d7622fc839da64c
Ivan Voitovych <col606v@gmail.com>
2022-01-26 09:52:16 +0200

    InterceptorsTestComponent

--- viewi-app/Components/Views/InterceptorsTest/InterceptorsTestComponent.php
+++ viewi-app/Components/Views/InterceptorsTest/InterceptorsTestComponent.php
@@ -3,6 +3,7 @@
 namespace Components\Views\InterceptorsTest;
 
 use Components\Models\PostModel;
+use Components\Services\Interceptors\AuthorizationInterceptor;
 use Components\Services\Interceptors\SessionInterceptor;
 use Viewi\BaseComponent;
 use Viewi\Common\HttpClient;
@@ -11,17 +12,20 @@ class InterceptorsTestComponent extends BaseComponent
 {
     public string $title = 'SSR + Interceptors with Deferred (async) response';
     public ?PostModel $post = null;
+    public string $message = '';
 
-    public function __init(int $id, HttpClient $http, SessionInterceptor $session)
+    public function __init(int $id, HttpClient $http, SessionInterceptor $session, AuthorizationInterceptor $auth)
     {
         $http
             ->with([$session, 'intercept'])
-            ->get("/api/posts/$id/async")->then(
+            ->with([$auth, 'intercept'])
+            ->get("/api/posts/$id/async/200")->then(
                 function (PostModel $post) {
                     $this->post = $post;
                     // print_r("http->get->then->RESOLVED\n");
                 },
                 function ($error) {
+                    $this->message = $error;
                     // print_r("http->get->then->ERROR\n");
                 }
             );
