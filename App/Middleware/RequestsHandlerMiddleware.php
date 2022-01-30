43d2e6babf1ef061671aee615e1f036fa318a49b
Ivan Voitovych <col606v@gmail.com>
2022-01-30 14:57:09 +0200

    RequestsHandlerMiddleware

--- App/Middleware/RequestsHandlerMiddleware.php
+++ App/Middleware/RequestsHandlerMiddleware.php
@@ -8,8 +8,8 @@ use React\Http\Message\Response;
 use React\Promise\Promise;
 use Throwable;
 use Viewi\App;
-use Viewi\BaseComponent;
 use Viewi\DI\Container;
+use Viewi\Routing\RouteItem;
 use Viewi\Routing\Router;
 use Viewi\WebComponents\IHttpContext;
 
@@ -17,7 +17,6 @@ class RequestsHandlerMiddleware
 {
     public function __invoke(ServerRequestInterface $request)
     {
-        // print_r([$request->getUri()->getPath(), $request->getMethod(), $request->getQueryParams()]);
         $match = Router::resolve($request->getUri()->getPath(), $request->getMethod());
         if (!$match) {
             return new Response(
@@ -33,7 +32,6 @@ class RequestsHandlerMiddleware
         $route = $match['route'];
         $action = $route->action;
         if (is_callable($action) && !is_string($action)) {
-            // print_r([$request->getUri()->getPath(), $request->getMethod(), $action, $match['params']]);
             if ($match['params']) {
                 $request = $request->withAttribute('params', $match['params']);
             }
@@ -42,65 +40,63 @@ class RequestsHandlerMiddleware
         // Viewi component
         return new Promise(function ($resolve, $reject) use ($request, $action, $match) {
             try {
-                /** @var BaseComponent $instance */
-                // $instance = new $action();
                 $container = new Container();
                 $httpContext = new ReactHttpContext($request);
                 $container->set(IHttpContext::class, $httpContext);
-                $directOutput = App::getEngine()->render($action, $match['params'], $container, function ($viewiResponse) use ($httpContext, $resolve) {
-                    // echo 'Viewi callback' . PHP_EOL;
-                    // callback here
-                    // $viewiResponse = $instance($match['params'], $container);
-                    $headers = $httpContext->getResponseHeaders();
-                    if (isset($headers['Location'])) {
-                        $resolve(
-                            new Response(
-                                302,
-                                $headers
-                            )
-                        );
-                        return;
-                    }
-                    // $viewiResponse = Router::handle($request->getUri()->getPath(), $request->getMethod(), $request->getQueryParams());
-                    // print_r(['response', $viewiResponse]);
-                    // print_r(['context', $request->getUri()->getPath(), $request->getMethod(), $httpContext->getResponseHeaders()]);
-                    if ($viewiResponse instanceof \Viewi\WebComponents\Response) {
-                        $resolve(
-                            new Response(
-                                $viewiResponse->StatusCode,
-                                array(
-                                    'Content-Type' => 'text/html'
-                                ) + $headers,
-                                $viewiResponse->Content
-                            )
-                        );
-                        return;
-                    } else if (is_string($viewiResponse)) {
-                        $resolve(
-                            new Response(
+                App::getEngine()->render(
+                    $action,
+                    $match['params'],
+                    $container,
+                    function ($viewiResponse) use ($httpContext, $resolve) {
+                        $headers = $httpContext->getResponseHeaders();
+                        if (isset($headers['Location'])) {
+                            $resolve(
+                                new Response(
+                                    302,
+                                    $headers
+                                )
+                            );
+                            return;
+                        }
+
+                        if ($viewiResponse instanceof \Viewi\WebComponents\Response) {
+                            $resolve(
+                                new Response(
+                                    $viewiResponse->StatusCode,
+                                    array(
+                                        'Content-Type' => 'text/html'
+                                    ) + $headers,
+                                    $viewiResponse->Content
+                                )
+                            );
+                            return;
+                        } else if (is_string($viewiResponse)) {
+                            $resolve(
+                                new Response(
+                                    200,
+                                    array(
+                                        'Content-Type' => 'text/html'
+                                    ) + $headers,
+                                    $viewiResponse
+                                )
+                            );
+                            return;
+                        } else { // json
+                            $resolve(new Response(
                                 200,
                                 array(
-                                    'Content-Type' => 'text/html'
+                                    'Content-Type' => 'application/json'
                                 ) + $headers,
-                                $viewiResponse
-                            )
-                        );
-                        return;
-                    } else { // json
-                        $resolve(new Response(
-                            200,
-                            array(
-                                'Content-Type' => 'application/json'
-                            ) + $headers,
-                            json_encode($viewiResponse)
-                        ));
-                        return;
+                                json_encode($viewiResponse)
+                            ));
+                            return;
+                        }
                     }
-                });
+                );
             } catch (Throwable $t) {
                 echo $t->getMessage() . "\n";
                 // echo $t->getTraceAsString();
-                // print_r($t);
+                // !! For production: Consider using $reject and don't output stack trace
                 $resolve(new Response(
                     500,
                     array(
